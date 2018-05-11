729d84d2388bfb73b58ee4f9c40ce60d6e1d64f7
Krati500 <38915844+Krati500@users.noreply.github.com>
2018-05-11 22:28:30 +0530

    Update ImageFormatting.py
    Fixed Some Bugs in Image Rotation Module.

--- ImageFormatting.py
+++ ImageFormatting.py
@@ -27,9 +27,11 @@ def select_img():
 def resize_func():
     try:
         path = root.filename
+        """
         index = path.rfind(r'/')
         append = path[index+1:]
         print(append)
+        """
         im = cv.imread(path)
 
         height, width = im.shape[:2]
@@ -37,13 +39,21 @@ def resize_func():
         divide=100/x
         thumbnail = cv.resize(im, (int(width/divide), int(height/divide)), interpolation = cv.INTER_AREA)
         path1()
-        newfilename = r'C:\Resized_Images\\new_'
+        """newfilename = r'C:\Resized_Images\\new_'
         newfilename = newfilename+append
+        """
 
-        cv.imshow(newfilename, thumbnail)
-        cv.imwrite(newfilename,thumbnail)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
+        if len(str(save_as.get())):
+            img_resize = (str(save_as.get()))+ '.jpg'
+            cv.imshow(img_resize, thumbnail)
+            cv.imwrite(img_resize,thumbnail)
+            cv.waitKey(0)
+            cv.destroyAllWindows()
+            tkinter.messagebox.showinfo('Success','Image Resized and Saved at \n C:\Resized_Images\ ')
+        else:
+            tkinter.messagebox.showinfo('Error','Please write Image Name First')
+
+
     except AttributeError:
         tkinter.messagebox.showinfo('Error','Please select an Image First')
 
@@ -71,25 +81,31 @@ def decrease_scale():
 def pdf_convert():
     try:
         path = root.filename
+        """
         index = path.rfind(r'/')
         append = path[index+1:]
-
         append = append[:-4]+'.pdf'
-        print(append)
-        newfilename = r'C:\Resized_Images\\new_'
-        newfilename = newfilename+append
-        filename = path
-        im = PIL.Image.open(filename)
-        PIL.Image.Image.save(im, newfilename, "PDF", resoultion=70.0)
-        tkinter.messagebox.showinfo('Success','PDF saved at \n C:\Resized_Images\ ')
+        print(append)"""
+
+        im = PIL.Image.open(path)
+        if len(str(save_as.get())):
+            img_to_pdf = (str(save_as.get()))+ '.pdf'
+            PIL.Image.Image.save(im, img_to_pdf, "PDF", resoultion=90.0)
+            tkinter.messagebox.showinfo('Success','PDF saved at \n C:\Resized_Images\ ')
+        else:
+            tkinter.messagebox.showinfo('Error','Please write Image Name First')
+
     except AttributeError:
         tkinter.messagebox.showinfo('Error','Please select an Image First')
+
+
+#Face Detection Functions
 
 def face_detect():
     try:
         face_cascade= cv.CascadeClassifier("haarcascade_frontalface_default.xml")
-
-        img = cv.imread("download.jpg")
+        path = root.filename
+        img = cv.imread(path)
         gray_img = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 
         faces = face_cascade.detectMultiScale(gray_img,
@@ -97,25 +113,106 @@ def face_detect():
         minNeighbors=5)
 
         for x,y,w,h in faces:
-            img = cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
-
-        print(type(faces))
-        print(faces)
+            img = cv.rectangle(img,(x,y),(x+w,y+h),(100,255,10),3)
 
 
-        cv.imshow("Gray",img)
-        cv.waitKey(5000)
-        path1()
         if len(str(save_as.get())):
             face_img = (str(save_as.get()))+ '.jpg'
+            path1()
+
+            cv.imshow("Gray",img)
+            cv.waitKey(4000)
+            cv.imwrite(face_img,img)
+            cv.destroyAllWindows()
+            tkinter.messagebox.showinfo('Success','Processed Image saved in \n C:\Resized_Images\ ')
         else:
             tkinter.messagebox.showinfo('Error','Please write Image Name First')
-        cv.imwrite(face_img,img)
-        cv.destroyAllWindows()
-        tkinter.messagebox.showinfo('Success','Processed Image saved in \n C:\Resized_Images\ ')
+    except AttributeError:
+        tkinter.messagebox.showinfo('Error','Please select an Image First')
+    except :
+        tkinter.messagebox.showinfo('Error','An unexpected Error Occured Run the Program Again')
+
+
+
+
+#Rotate Image Functions
+
+def rotate_90():
+    try:
+        path = root.filename
+        path1()
+        if len(str(save_as.get())):
+            img_rotate_90 = (str(save_as.get()))+ '.jpg'
+            img = Image.open(path)
+            img2 = img.rotate(90)
+            img2.save(img_rotate_90)
+            tkinter.messagebox.showinfo('Success','Image Rotated by 90 and Saved at \n C:\Resized_Images\ ')
+
+        else:
+            tkinter.messagebox.showinfo('Error','Please write Image Name First')
+
     except AttributeError:
         tkinter.messagebox.showinfo('Error','Please select an Image First')
 
+def rotate_180():
+    try:
+        path = root.filename
+        path1()
+        if len(str(save_as.get())):
+            img_rotate_180 = (str(save_as.get()))+ '.jpg'
+            img = Image.open(path)
+            img2 = img.rotate(180)
+            img2.save(img_rotate_180)
+            tkinter.messagebox.showinfo('Success','Image Rotated by 180 and Saved at \n C:\Resized_Images\ ')
+
+        else:
+            tkinter.messagebox.showinfo('Error','Please write Image Name First')
+    except AttributeError:
+        tkinter.messagebox.showinfo('Error','Please select an Image First')
+
+
+def rotate_270():
+    try:
+        path = root.filename
+        path1()
+        if len(str(save_as.get())):
+            img_rotate_270 = (str(save_as.get()))+ '.jpg'
+            img = Image.open(path)
+            img2 = img.rotate(270)
+            img2.save(img_rotate_270)
+            tkinter.messagebox.showinfo('Success','Image Rotated by 270 and Saved at \n C:\Resized_Images\ ')
+
+        else:
+            tkinter.messagebox.showinfo('Error','Please write Image Name First')
+    except AttributeError:
+        tkinter.messagebox.showinfo('Error','Please select an Image First')
+
+
+#Take a PHOTO
+def take_photo():
+    try:
+        cap = cv.VideoCapture(0)
+
+        while(True):
+            ret, frame = cap.read()
+            rgb = cv.cvtColor(frame, cv.COLOR_BGR2BGRA)
+
+            cv.imshow('frame', rgb)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                path1()
+                if len(str(save_as.get())):
+                    photo_webcam = (str(save_as.get()))+ '.jpg'
+                    out = cv.imwrite(photo_webcam, frame)
+                    break
+                else:
+                    tkinter.messagebox.showinfo('Error','Please write Image Name First')
+                break
+
+        cap.release()
+        cv.destroyAllWindows()
+        tkinter.messagebox.showinfo('Success','Photo from webcam saved.')
+    except:
+        tkinter.messagebox.showinfo('Error','WebCam Might be Absent.')
 
 
 root = Tk()
@@ -126,117 +223,61 @@ b1=tk.Button(root,text="Select Image", width = 15,command=select_img)
 b1.grid(row=0,column=0)
 
 
-l1=Label(root,text="RESIZE AN IMAGE")
-l1.grid(row=1,column=2)
-
-b2=tk.Button(root,text="+", width = 15,command=increase_scale)
-b2.grid(row=2,column=0)
-
-list1=Listbox(root, height=1, width=10)
-list1.grid(row=2,column=1)
-list1.insert(0,"90")
-
-b3=tk.Button(root,text="-", width = 15,command=decrease_scale)
-b3.grid(row=2,column=2)
-
-b4=tk.Button(root,text="Resize Image", width = 15,command=resize_func)
-b4.grid(row=2,column=3)
-
-#Image to PDF
-l2=Label(root,text="IMAGE to PDF CONVERTER")
-l2.grid(row=3,column=2)
-
-b5=tk.Button(root,text="Image to PDF", width = 15,command=pdf_convert)
-b5.grid(row=4,column=2)
-
-l3=Label(root,text="DETECT FACE FROM IMAGE")
-l3.grid(row=5,column=2)
-
 l4=Label(root,text="Save file as:")
-l4.grid(row=6,column=0)
+l4.grid(row=1,column=0)
 
 global e1
 save_as =StringVar()
 e1=Entry(root,textvariable=save_as, width=35)
-e1.grid(row=6,column=2,columnspan=15)
+e1.grid(row=1,column=2,columnspan=15)
+
+
+l1=Label(root,text="RESIZE AN IMAGE")
+l1.grid(row=2,column=2)
+
+b2=tk.Button(root,text="+", width = 15,command=increase_scale)
+b2.grid(row=3,column=0)
+
+list1=Listbox(root, height=1, width=10)
+list1.grid(row=3,column=1)
+list1.insert(0,"90")
+
+b3=tk.Button(root,text="-", width = 15,command=decrease_scale)
+b3.grid(row=3,column=2)
+
+b4=tk.Button(root,text="Resize Image", width = 15,command=resize_func)
+b4.grid(row=3,column=3)
+
+#Image to PDF
+l2=Label(root,text="IMAGE to PDF CONVERTER")
+l2.grid(row=4,column=2)
+
+b5=tk.Button(root,text="Image to PDF", width = 15,command=pdf_convert)
+b5.grid(row=5,column=2)
+
+l3=Label(root,text="DETECT FACE FROM IMAGE")
+l3.grid(row=6,column=2)
 
 b6=tk.Button(root,text="DETECT FACE ", width = 15,command=face_detect)
 b6.grid(row=7,column=2)
 
 
+l5=Label(root,text="ROTATE AN IMAGE")
+l5.grid(row=8,column=2)
+
+b7=tk.Button(root,text="ROTATE 90 DEG ", width = 15,command=rotate_90)
+b7.grid(row=9,column=0)
+
+b8=tk.Button(root,text="ROTATE 180 DEG ", width = 15,command=rotate_180)
+b8.grid(row=9,column=1)
+
+b9=tk.Button(root,text="ROTATE 270 DEG ", width = 15,command=rotate_270)
+b9.grid(row=9,column=2)
+
+b10=tk.Button(root,text="TAKE A PHOTO (Press Q)", width = 30,command=take_photo)
+b10.grid(row=10,column=2)
+
 # Take a Photo From WebCam
 # Image Rotate
 # Detect Face from from images
 root.mainloop()
-
-"""
-def rotate_90():
-    try:
-        path = root.filename
-
-        index = path.rfind(r'/')
-        append = path[index+1:]
-        print(append)
-        img = imread(path)
-        path1()
-        newfilename = r'C:\Resized_Images\\new_'
-        newfilename = newfilename + append
-        (h, w) = img.shape[:2
-                    ]
-# calculate the center of the image
-        center = (w / 2, h / 2)
-
-        angle90 = 90
-        angle180 = 180
-        angle270 = 270
-
-        scale = 1.0
-
-        # Perform the counter clockwise rotation holding at the center
-        # 90 degrees
-        M = cv.getRotationMatrix2D(center, angle90, scale)
-        rotated90 = cv.warpAffine(img, M, (h, w))
-        rotate_img = rotate(img, 90)
-        cv.imwrite(newfilename,rotate_img)
-
-        tkinter.messagebox.showinfo('Success','Image Rotated by 90 and Saved at \n C:\Resized_Images\ ')
-    except AttributeError:
-        tkinter.messagebox.showinfo('Error','Please select an Image First')
-
-def rotate_180():
-    try:
-        path = root.filename
-
-        index = path.rfind(r'/')
-        append = path[index+1:]
-        print(append)
-        img = imread(path)
-        path1()
-        newfilename = r'C:\Resized_Images\\new_'
-        newfilename = newfilename + append
-        rotate_img = rotate(img, 180)
-        cv.imshow(newfilename,rotate_img)
-        cv.imwrite(newfilename,rotate_img)
-
-        tkinter.messagebox.showinfo('Success','Image Rotated by 180 and Saved at \n C:\Resized_Images\ ')
-    except AttributeError:
-        tkinter.messagebox.showinfo('Error','Please select an Image First')
-
-def rotate_270():
-    try:
-        path = root.filename
-
-        index = path.rfind(r'/')
-        append = path[index+1:]
-        print(append)
-        img = imread(path)
-        path1()
-        newfilename = r'C:\Resized_Images\\new_'
-        newfilename = newfilename + append
-        rotate_img = rotate(img, 270)
-        cv.imwrite(newfilename,rotate_img)
-
-        tkinter.messagebox.showinfo('Success','Image Rotated by 270 and Saved at \n C:\Resized_Images\ ')
-    except AttributeError:
-        tkinter.messagebox.showinfo('Error','Please select an Image First')
-"""
